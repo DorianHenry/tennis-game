7968c97feb694bfa70699161b231ae5b701c9fba
Dorian Henry <d.henry@synchrone.be>
2023-05-20 21:07:25 +0200

    refactor: get types to functions

--- src/enums.ts
+++ src/enums.ts
@@ -1,22 +1,4 @@
-import { AvatarId, Point } from "./types";
-
 export enum GameState {
   ONGOING,
   FINISH,
 }
-
-export const scrorePoints = new Map<Point, string>([
-  [0, "0"],
-  [1, "15"],
-  [2, "30"],
-  [3, "40"],
-  [4, "AD"],
-]);
-
-export const avatarMap = new Map<AvatarId, string>([
-  [1, "girl-1.svg"],
-  [2, "girl-2.svg"],
-  [3, "girl-3.svg"],
-  [4, "girl-4.svg"],
-  [5, "girl-5.svg"],
-]);
--- src/functions/string.ts
+++ src/functions/string.ts
@@ -1,4 +1,5 @@
 import { GameState } from "../enums";
+import { AvatarId, Point } from "../types";
 
 export const gameStateMap = new Map<GameState, string>([
   [GameState.ONGOING, "En cours"],
@@ -7,4 +8,47 @@ export const gameStateMap = new Map<GameState, string>([
 
 export function getStateByNumber(state: GameState) {
   return gameStateMap.get(state);
+}
+
+export const scrorePoints = new Map<Point, string>([
+  [0, "0"],
+  [1, "15"],
+  [2, "30"],
+  [3, "40"],
+  [4, "AD"],
+]);
+
+export function getScorePoint(scorePoint: Point) {
+  return scrorePoints.get(scorePoint);
+}
+
+export const avatarMap = new Map<AvatarId, string>([
+  [1, "/avatars/girl-1.svg"],
+  [2, "/avatars/girl-2.svg"],
+  [3, "/avatars/girl-3.svg"],
+  [4, "/avatars/girl-4.svg"],
+  [5, "/avatars/girl-5.svg"],
+]);
+
+export function getAvatar(avatarId: AvatarId) {
+  return avatarMap.get(avatarId);
+}
+
+/**
+ * Generate className with different arguments boolean null and string
+ */
+export function classNames(...classnames: unknown[]) {
+  return classnames
+    .filter((classname) => classname !== null && classname !== false)
+    .join(" ");
+}
+
+/**
+ * Return a string with minimum two digits
+ */
+export function getLocalTwoNumber(number: number) {
+  return number.toLocaleString(undefined, {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
 }
