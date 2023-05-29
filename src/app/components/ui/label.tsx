64a5f4d1b83ea6665e32f767d5af459c6fc179d2
Dorian Henry <d.henry@synchrone.be>
2023-05-29 19:54:04 +0200

    fix: git bug case senstitive remove cache
    Git didn't changed the names files beacause not case sensitive
    Solution : git rm --cache fileName

--- src/app/components/ui/label.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react';
-export type LabelType = 'default' | 'primary' | 'secondary';
-type Props = {
-  type?: LabelType;
-};
-export function Label({ children, type = 'default' }: PropsWithChildren<Props>) {
-  return <span className={`label label--${type}`}>{children}</span>;
-}
